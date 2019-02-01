a6d947f34493e755c4d08aee9689ab53166b4ade
rstutalo <richardtutalo@gmail.com>
2019-01-31 21:26:42 -0700

    ajax api GET request working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,38 @@
 // APP VARIABLES - Actors
 
-var topics = ["Robert DiNero", "Al Pacino", "Nicholas Cage", "Josh Brolin", "Ryan Renolds", "Hugh Jackman", "Michelle Pfier", "Jennifer Lawrence", "Melissa McCarthy", "Robert Downey Jr.", "Marc Ruffalo"];
+var actors = ["Robert DiNero", "Al Pacino", "Nicholas Cage", "Josh Brolin", "Ryan Renolds", "Hugh Jackman", "Michelle Pfier", "Jennifer Lawrence", "Melissa McCarthy", "Robert Downey Jr.", "Marc Ruffalo"];
 
 //function to dynamically create buttons on html
 function buttonCreate() {
     $("#buttons").empty();
-    for (var i = 0; i < topics.length; i++) {
+    for (var i = 0; i < actors.length; i++) {
         var btn = $("<button>");
         btn.addClass("actor-button");
-        btn.attr("data-name", topics[i]);
+        btn.attr("data-name", actors[i]);
         $("#buttons").append(btn);
     };
-}
-
-// on click function on button to interact with giphy API
-function showGif() {
-    for (var i = 0; i < topics.length; i++) {
-        // on click event listener
-    }   
-        //call ajax method here so it runs on each button click
-        // reference value of button on click so api knows what gifs to grab
-        // limit search to 10 items for each button
-        // 
 }
 
 
 
 //API CREDS
+function gifInfo() {
+    var apiKey = "I4J6THgSbr8lVICfhcpWKlgKpS957dTP"; 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + actors + "&api_key=" + apiKey + "&rating&limit=10";
 
-var apiKey = "I4J6THgSbr8lVICfhcpWKlgKpS957dTP";
-var queryURL = "";
+    //AJAX Request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        // var actorDiv = $("<div class='actor'>");
+        // var rating = response.rating;
+        // var ratingScore
+    });
+}
+
 
 //FUNCTION CALLS
 buttonCreate();
+gifInfo();
